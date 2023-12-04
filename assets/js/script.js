e4e3b78eec968a91427bc7d99ee4ae6b260ca0b6
delanohendrix <116620550+delanohendrix@users.noreply.github.com>
2023-12-04 16:24:08 -0500

    Updated script to add support for other browsers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,42 @@
-import data from "../../data.json" assert { type: "json" };
-const score = document.querySelector(".score-container");
-const parent = document.querySelector(".summary-categories");
+fetch("../../data.json")
+  .then((response) => response.json())
+  .then((data) => genScores(data));
 
-let sum = 0;
-for (let i = 0; i < data.length; i++) {
-  sum += data[i].score;
-}
-let avgScore = document.createElement("p");
-avgScore.textContent = parseInt(sum / data.length);
-score.prepend(avgScore);
+function genScores(data) {
+  const score = document.querySelector(".score-container");
+  const parent = document.querySelector(".summary-categories");
 
-for (let i = 0; i < data.length; i++) {
-  let div = document.createElement("div");
-  let categoryName = data[i].category.toLowerCase();
-  div.classList.add("category", categoryName);
+  let sum = 0;
 
-  let img = document.createElement("img");
-  img.setAttribute("src", data[i].icon);
-  img.setAttribute("alt", "");
-  div.appendChild(img);
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i].score;
+  }
+  let avgScore = document.createElement("p");
+  avgScore.textContent = parseInt(sum / data.length);
+  score.prepend(avgScore);
 
-  let p = document.createElement("p");
-  p.textContent = data[i].category;
-  div.appendChild(p);
+  for (let i = 0; i < data.length; i++) {
+    let div = document.createElement("div");
+    let categoryName = data[i].category.toLowerCase();
+    div.classList.add("category", categoryName);
 
-  let num = document.createElement("p");
-  num.classList.add("score");
-  let boldScore = document.createElement("STRONG");
-  boldScore.textContent = data[i].score;
-  num.appendChild(boldScore);
-  num.insertAdjacentText("beforeend", " / 100");
-  div.appendChild(num);
+    let img = document.createElement("img");
+    img.setAttribute("src", data[i].icon);
+    img.setAttribute("alt", "");
+    div.appendChild(img);
 
-  parent.appendChild(div);
+    let p = document.createElement("p");
+    p.textContent = data[i].category;
+    div.appendChild(p);
+
+    let num = document.createElement("p");
+    num.classList.add("score");
+    let boldScore = document.createElement("STRONG");
+    boldScore.textContent = data[i].score;
+    num.appendChild(boldScore);
+    num.insertAdjacentText("beforeend", " / 100");
+    div.appendChild(num);
+
+    parent.appendChild(div);
+  }
 }
